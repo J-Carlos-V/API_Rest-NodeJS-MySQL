63888b1241cfa9a276e6c2d94db4a51e9d220798
Juan_Carlos_V <juancarlos.vivero@utxicotepec.edu.mx>
2021-11-03 03:14:39 -0600

    configuracion de bd

--- src/database.js
+++ src/database.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql');
 
 const mysqlConnection =  mysql.createConnection({
-    host: 'sql10.freesqldatabase.com',
+    host: 'us-cdbr-east-04.cleardb.com',
     port: '3306',
-    user: 'sql10448653',
-    password: 'cxuF62ch3n',
-    database: 'sql10448653'
+    user: 'bd27688926f172',
+    password: 'b54f6127',
+    database: 'heroku_3365cf75d9a5ad5'
 });
+// mysql://bd27688926f172:b54f6127@us-cdbr-east-04.
 
 mysqlConnection.connect(function (err){
     if(err){
